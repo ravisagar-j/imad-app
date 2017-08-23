70cc70ff3844e0ba2d94d9c10ceed61d0c88ae69
Ravi J <rjshp1234@gmail.com>
2017-08-23 12:43:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
     title : 'Article one | Ravi',
     heading : 'Article one',
     date : 'aug 22 2017',
@@ -17,21 +17,52 @@ var content = {
      <p> This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article </p> `
 
 };
+function createTemplate (data) {
+    var title = data.title;
+    var date =  data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+var htmlTemplate = `<!DOCTYPE html>
+  <head>
+     <title> ${title}</title>
+     <meta name="viewport" contenr="device-width, initial-scale=1" />
+     <link href="/ui/style.css" rel="stylesheet" />
+      </head>
+  <body>
+      <div class="container">
+      <div>
+          <a href="/">home</a>
+      </div>
+      <hr/>
+      <h3>
+          ${heading}
+      </h3>
+      <div>
+          ${date}
+      </div>
+      <div>
+          ${content}
+      </div>
+      </div>
+     </body>
+   </html> `;
+   return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-   res.send('Article one requested and will be served here');
+app.get('/article-one', function(req, res){
+   res.send(createTemplate(articleOne));
 });
 
-app.get('article-two', function(req, res){
+app.get('/article-two', function(req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('article-three', function(req, res){
+app.get('/article-three', function(req, res){
      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
    res.send('Article three requested and will be served here');
    
