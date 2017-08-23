2e8417ee77c315c283fe89241a46411f1f87133c
Ravi J <rjshp1234@gmail.com>
2017-08-23 13:16:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-var articleOne : {
+  'article-One' : {
     title : 'Article one | Ravi',
     heading : 'Article one',
     date : 'aug 22 2017',
@@ -18,22 +18,23 @@ var articleOne : {
      <p> This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article. This is my first article </p> `
 },
 
-var articleTwo : {
+  'article-Two' : {
     title : 'Article two | Ravi',
     heading : 'Article two',
     date : 'sep 22 2017',
     content : ` 
-         <p> This is my second article. This is my second article. </p>
+         <p> This is my second article. This is my second article. </p> `
     
 },
 
-var articleThree : {
+  'article-Three' : {
     title : 'Article three | Ravi',
     heading : 'Article three',
     date : 'jul 22 2018',
     content : ` 
-         This is my third article. This is my third article. 
- },
+         <p>This is my third article. This is my third article. </p>`
+ }
+};
  
 function createTemplate (data) {
     var title = data.title;
@@ -41,7 +42,7 @@ function createTemplate (data) {
     var heading = data.heading;
     var content = data.content;
     
-var htmlTemplate = `<!DOCTYPE html>
+var htmlTemplate = `<html>
   <head>
      <title> ${title}</title>
      <meta name="viewport" contenr="device-width, initial-scale=1" />
@@ -72,8 +73,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res){
-   res.send(createTemplate(articleOne));
+app.get('/:articleName', function(req, res){
+    //articleName == article-one
+    //articles[articleName] == {} content object for article one
+     var articleName = req.param.articleName;
+   res.send(createTemplate(articles[articleName]);
+     
+    
 });
 
 app.get('/article-two', function(req, res){
