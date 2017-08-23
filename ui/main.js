7a3ba6bce7171c4637ad3bd6ec06145d4313dbfb
Ravi J <rjshp1234@gmail.com>
2017-08-23 18:32:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,15 @@
 console.log('Loaded!');
 //change the text of the main-text division
-var element = document.getElementById('main text'
-   );
+var element = document.getElementById('main text');
    element.innerHTML = 'new value';
    
 //move the image
-var img = document.getElementById('img'); 
+var img = document.getElementById('img');
+var marginLeft = 0;
+function moveRight(){
+marginLeft = marginLeft + 10;
+img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-     img.style.marginLeft = '100px'
+    var interval = setInterval(moveRight, 100);
 }
