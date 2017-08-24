b3d17227716975ff11d69a6c37ce3c3e658a2b67
Ravi J <rjshp1234@gmail.com>
2017-08-24 14:17:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool
+ 
+ var config = {
+     user: 'ravij1234',
+     database: 'rjshp1234',
+     host: 'db.imad.hasura-app.io',
+     port: '5432',
+     password: process.env.DB_PASSWORD
+ }
 var app = express();
 app.use(morgan('combined'));
 
@@ -72,9 +80,17 @@ var htmlTemplate = `<html>
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var Pool = new Pool(config);
 app.get('/test-db', function (reg, res) {
   //make a select request    
+pool.query('SELECT * FROM test', function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+        } else {
+            res.send(JSON,strngify(result));
+            
+        }
+    });
 });
 
 var counter = 0;
