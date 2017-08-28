44dc0d6f4bff762ccc40d2ef3c20f184a5620341
Ravi J <rjshp1234@gmail.com>
2017-08-28 14:59:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,51 @@
-console.log('Loaded!');
-//change the text of the main-text division
-var element = document.getElementById('main text');
-   element.innerHTML = 'new value';
-   
-//move the image
-var img = document.getElementById('img');
-var marginLeft = 0;
-function moveRight(){
-marginLeft = marginLeft + 10;
-img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+// COunter code
+
+var button = document.getElementById('counter');
+
+button.onclick = function () {
+  //create a request object
+   var request = new XMLHttpRequest();
+
+  //Capture the response and store it in a variable
+
+   request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+       //take some action
+      if (request.status === 200) {
+         var counter = request.responseTest;
+         var span = document.getElementById('count');
+             span.innerHTML = counter.toString();
+        }
+   }
+    //Not done yet
+  };
+
+request.open('GET', 'http://rjshp1234.imad.hasura-app.io/counter', true);
+request.send(null);
 };
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  var request = new XMLHttpRequest();
+  
+request.onreadystatechange = function () {
+  if (request.readyState === XMLHttpRequest.DONE) {
+      if(request.status === 200) {
+         var names = request.responseText;
+             names = JSON.parse(names);
+         var list = '';
+          for(var i=0; i<names.lenght; i++) {
+             list += '<li>' + names[i] + '</li>';
+           }
+          var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+       }
+   }
+};
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+request.open('GET','http://rjshp1234.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
+};
+
